fc830ed81a152290a51796afae77b9fd388bcb54
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-01-13 16:56:22 +0330

    completed project entity

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,7 +1,15 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+import StatusEnum from "../enums/status.enum";
 
 @Entity({ name: "projects" })
 export class Project {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: "enum", enum: StatusEnum, default: StatusEnum.Enable })
+  status: StatusEnum;
+
+  @Column()
+  title: string;
 }
